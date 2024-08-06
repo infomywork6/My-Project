3ee4c150607f4359c53856dd1e8472c7371a3ab2
Prashant <162093069+infomywork6@users.noreply.github.com>
2024-08-06 06:55:33 +0530

    Add movie list UI

--- app/src/main/java/com/prashant/my_projects/MainActivity.kt
+++ app/src/main/java/com/prashant/my_projects/MainActivity.kt
@@ -4,26 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.prashant.my_projects.ui.theme.MVIDemoTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MVIDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MVIScreen(viewModel = mainViewModel)
                 }
             }
         }
@@ -31,17 +48,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MVIScreen(viewModel: MainViewModel) {
+    val state by viewModel.state.collectAsState()
+    LaunchedEffect(viewModel) {
+        viewModel.handleIntent(MovieIntent.LoadMovie)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        when {
+            state.loading -> {
+                CircularProgressIndicator(modifier = Modifier.align(alignment = Alignment.CenterHorizontally))
+            }
+
+            state.error != null -> {
+                Text(text = "Error: ${state.error}", color = Color.Red)
+            }
+
+            else -> {
+                MovieListUI(state.movie)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MVIDemoTheme {
-        Greeting("Android")
+fun MovieListUI(movie: List<Movie>) {
+    LazyColumn {
+        items(movie) { movie ->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .shadow(4.dp, RoundedCornerShape(8.dp))
+            ) {
+                Text(text = movie.title, modifier = Modifier.padding(4.dp))
+                Text(text = movie.year, modifier = Modifier.padding(4.dp))
+            }
+        }
     }
 }
